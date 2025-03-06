49401fc7c2dfd594aa387c30829165dec44d992b
Nícolas <nicolasauersvalt@gmail.com>
2025-03-06 00:24:01 -0300

    botoes

--- page/main.py
+++ page/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import json
 import os
+import webbrowser
 
 text_path = os.path.join('assets', 'textos', 'main.json')
 
@@ -8,40 +9,62 @@ text_path = os.path.join('assets', 'textos', 'main.json')
 with open(text_path, 'r', encoding='utf-8') as f:
     dados = json.load(f)
 
-def inicio():
-    
-    # Exibir uma imagem a partir de um arquivo local
-    imagem_path = "assets/LambdaLabs.png"
+    # Função para exibir informações dos pesquisadores
+def exibir_pesquisador(nome, mentor, projetos, linkedin, github):
+    st.write(f"**{nome}**")
+    st.write(f"- **Mentor:** {mentor}")
+    st.write(f"- **Projetos:** {projetos}")
 
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        st.link_button("🔗 Linkedin", linkedin)
+
+    with col2:
+        st.link_button("🐙 Portfólio", github)
+    
+    st.markdown("---")
+
+def inicio():
     st.title("Bem-vindo ao Laboratório Lambda")
 
+    # Exibir a imagem principal
+    imagem_path = "assets/LambdaLabs.png"
     st.image(imagem_path, width=400)
-    
+
     # Exibir mensagem inicial
     st.write(dados['mensagem_inicial'])
     st.markdown("---")
 
-        # Exibir dados do Pesquisador Principal
-    st.write("**Pesquisador Principal: {}**".format(dados['pesquisador_principal']))
-    st.write("- Mentor: {}".format(dados['mentor_principal']))
-    st.write("- Projetos: {}".format(dados['projetos_principal']))
-    st.markdown("LinkedIn: [{}]({})".format(dados['pesquisador_principal'], dados['linkedin_principal']))
-    st.markdown("Portfólio: [{}]({})".format(dados['pesquisador_principal'], dados['portfolio_principal']))
-    st.markdown("---")
+    # Pesquisador Principal
+    exibir_pesquisador(
+        dados['pesquisador_principal'],
+        dados['mentor_principal'],
+        dados['projetos_principal'],
+        dados['linkedin_principal'],
+        dados['portfolio_principal']
+    )
 
-    # Exibir dados do Pesquisador Associado
-    st.write("**Pesquisador Associado: {}**".format(dados['pesquisador_associado']['nome']))
-    st.write("- Mentor: {}".format(dados['pesquisador_associado']['mentor']))
-    st.write("- Projetos: {}".format(dados['pesquisador_associado']['projetos']))
-    st.markdown("LinkedIn: [{}]({})".format(dados['pesquisador_associado']['nome'], dados['pesquisador_associado']['linkedin']))
-    st.markdown("---")
+    # Pesquisador Associado
+    associado = dados['pesquisador_associado']
+    exibir_pesquisador(
+        associado['nome'],
+        associado['mentor'],
+        associado['projetos'],
+        "https://www.linkedin.com/in/gabriel-trevisani-a811131b5/",
+        "https://github.com/fermathematician"
+    )
 
-    # Exibir dados do Pesquisador Assistente
-    st.write("**Pesquisador Assistente: {}**".format(dados['pesquisador_assistente']['nome']))
-    st.write("- Mentor: {}".format(dados['pesquisador_assistente']['mentor']))
-    st.write("- Projetos: {}".format(dados['pesquisador_assistente']['projetos']))
-    st.markdown("---")
+    # Pesquisador Assistente
+    assistente = dados['pesquisador_assistente']
+    exibir_pesquisador(
+        assistente['nome'],
+        assistente['mentor'],
+        assistente['projetos'],
+        "https://www.linkedin.com/in/pedro-nascimento-4a35982b0/",
+        "https://github.com/nascpedro"
+    )
 
-    # Exibir dados do Revisor
-    st.write("**Revisor: {}**".format(dados['revisor']))
+    # Exibir o Revisor
+    st.write(f"**Revisor:** {dados['revisor']}")
     st.markdown("---")
